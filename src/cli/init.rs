1bc61bf6f8e77befc45beb09e7c76a6f11989929
Nyakku Shigure <sigure.qaq@gmail.com>
2025-12-15 01:47:47 +0800

    🐛 fix: add random suffix to meowda script to avoid conflict between multiprocess (#44)

--- src/cli/init.rs
+++ src/cli/init.rs
@@ -72,7 +72,7 @@ fn inject_init_script(shell_profile: &str) -> Result<()> {
     let init_script = format!(
         r#"
 # {meowda_init_comment}
-MEOWDA_TMP_SCRIPT="/tmp/meowda-init-tmp.sh"
+MEOWDA_TMP_SCRIPT="/tmp/meowda-init-tmp-$RANDOM.sh"
 meowda generate-init-script > "$MEOWDA_TMP_SCRIPT"
 source "$MEOWDA_TMP_SCRIPT"
 rm -f "$MEOWDA_TMP_SCRIPT"
